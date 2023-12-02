70b658e893987f58af491d9b279d7cac4db6061d
jameshaobam <haobamjamess8@gmail.com>
2023-12-02 11:35:31 +0530

    added search feature in datawidget

--- lib/widgets/datawidget.dart
+++ lib/widgets/datawidget.dart
@@ -4,7 +4,7 @@ import '../data/guarantee.dart';
 import 'gridview_item.dart';
 import 'modalgraph.dart';
 
-class DataWidget extends StatelessWidget {
+class DataWidget extends StatefulWidget {
   const DataWidget({
     super.key,
     required this.dataList,
@@ -13,34 +13,75 @@ class DataWidget extends StatelessWidget {
   final List<Guarantee> dataList;
 
   @override
+  State<DataWidget> createState() => _DataWidgetState();
+}
+
+class _DataWidgetState extends State<DataWidget> {
+  late List<Guarantee> filteredDataList;
+
+  @override
+  void initState() {
+    super.initState();
+    filteredDataList = List.from(widget.dataList);
+  }
+
+  void filterData(String searchTerm) {
+    setState(() {
+      if (searchTerm.isEmpty) {
+        filteredDataList = List.from(widget.dataList);
+      } else {
+        filteredDataList = widget.dataList
+            .where((guarantee) => guarantee.statesUts
+                .toLowerCase()
+                .contains(searchTerm.toLowerCase()))
+            .toList();
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Number of columns in the grid
-          crossAxisSpacing: 8.0, // Spacing between columns
-          mainAxisSpacing: 8.0, // Spacing between rows
+    return Column(
+      children: [
+        TextField(
+          onChanged: filterData,
+          decoration: const InputDecoration(
+            labelText: 'Search by State/Ut',
+            prefixIcon: Icon(Icons.search),
+          ),
         ),
-        itemCount: dataList.length,
-        itemBuilder: (context, index) {
-          if (dataList[index].srNo == "Total") {
-            return null;
-          }
-          return GestureDetector(
-            onTap: () {
-              showModalBottomSheet(
-                isScrollControlled: true,
-                isDismissible: false,
-                context: context,
-                builder: (context) {
-                  return ModalGraph(
-                    index: index,
-                    guaranteeList: dataList,
+        Expanded(
+          child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // Number of columns in the grid
+              crossAxisSpacing: 8.0, // Spacing between columns
+              mainAxisSpacing: 8.0, // Spacing between rows
+            ),
+            itemCount: filteredDataList.length,
+            itemBuilder: (context, index) {
+              if (filteredDataList[index].srNo == "Total") {
+                return Container(); // Return an empty container or a placeholder widget if needed
+              }
+              return GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    isScrollControlled: true,
+                    isDismissible: false,
+                    context: context,
+                    builder: (context) {
+                      return ModalGraph(
+                        index: index,
+                        guaranteeList: filteredDataList,
+                      );
+                    },
                   );
                 },
+                child: GridItem(guarantee: filteredDataList[index]),
               );
             },
-            child: GridItem(guarantee: dataList[index]),
-          );
-        });
+          ),
+        ),
+      ],
+    );
   }
 }
